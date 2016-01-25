c6741d0865b4f66298cb14da015288f3d25757cb
cornellio <peter.cornell@me.com>
2016-01-24 16:11:40 -0800

    Update site manifest

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,8 +1,25 @@
 # Node definitions
 
 node default {
+  include stdlib
 }
 
-node 'web' {
-  include motd
+node 'app' {
+
+  class { 'java': }->
+  class { 'tomcat::params':
+    http_connector_port => '9000',
+  }
+  class { 'tomcat::install': }
+  class { 'tomcat::config': }
+  class { 'tomcat::service': }
+
+  include '::mysql::server'
+
+}
+
+node 'db' {
+
+  include '::mysql::server'
+
 }
