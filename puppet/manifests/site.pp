6dbb313dc3bddd3f757a112c8bca718def9690b9
cornellio <peter.cornell@me.com>
2016-01-25 14:27:11 -0800

    Update nodes definition for testing mysql datasource params

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,5 @@
 # Node definitions
-
+#
 node default {
   include stdlib
 }
@@ -8,18 +8,23 @@ node 'app' {
 
   class { 'java': }->
   class { 'tomcat::params':
-    http_connector_port => '9000',
+    http_connector_port         => '9000',
+    mysql_datasource_name       => 'MySqlGBDS',
+    mysql_datasource_host       => '172.16.10.3',
+    mysql_datasource_dbname     => 'grannydb',
+    mysql_datasource_username   => 'demo',
+    mysql_datasource_password   => 'demodemo',
   }
   class { 'tomcat::install': }
   class { 'tomcat::config': }
   class { 'tomcat::service': }
 
-  include '::mysql::server'
+  include 'mysql::server'
 
 }
 
 node 'db' {
 
-  include '::mysql::server'
+  include 'mysql::server'
 
 }
