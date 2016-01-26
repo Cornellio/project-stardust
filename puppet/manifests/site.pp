71007b6d92025780526f14fd410cee0a8593b58f
cornellio <peter.cornell@me.com>
2016-01-26 15:53:09 -0800

    Update nodes definition with nginx configs, adjust mysql configs

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -4,13 +4,13 @@ node default {
   include stdlib
 }
 
-node 'app' {
+node 'app01' {
 
   class { 'java': }->
   class { 'tomcat::params':
     http_connector_port         => '9000',
     mysql_datasource_name       => 'MySqlGBDS',
-    mysql_datasource_host       => '172.16.10.3',
+    mysql_datasource_host       => 'localhost',
     mysql_datasource_dbname     => 'grannydb',
     mysql_datasource_username   => 'demo',
     mysql_datasource_password   => 'demodemo',
@@ -19,12 +19,42 @@ node 'app' {
   class { 'tomcat::config': }
   class { 'tomcat::service': }
 
-  include 'mysql::server'
+  include'nginx'
 
-}
+  nginx::resource::upstream { 'app01.stardust.net':
+    members => [
+      'localhost:9000',
+    ],
+  }
+  nginx::resource::vhost { 'www.stardust.net':
+    proxy => 'http://app01.stardust.net',
+  }
 
-node 'db' {
+  # Mysql server configuration with custom options for my.cnf,
+  # with an example of changing the bind address
+  $override_options = {
+    'mysqld' => {
+      'bind-address' => '172.21.10.2'
+    }
+  }
+  class { '::mysql::server':
+    root_password            => 'planet!0rbit@l',
+    remove_default_accounts  => true,
+    override_options         => $override_options,
+  }
 
-  include 'mysql::server'
+  # Create database for the Address Book app
+  mysql::db { 'grannydb':
+    user     => 'demo',
+    password => 'demodemo',
+    host     => 'localhost',
+    grant    => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+  }
+
+  # Firewall rules
+  firewall { '000 accept all icmp requests':
+    proto  => 'icmp',
+    action => 'accept',
+  }
 
 }
